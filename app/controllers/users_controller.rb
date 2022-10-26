a7961f334d1cfe7ef2ff0c08b54192c8e9b8c7e7
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-10-25 21:00:29 -0400

    Begin connection to backend to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
   def new;end
 
   def create
-    require 'pry'; binding.pry
+    new_user = user_params 
+    # UserFacade.create_user(user_params)
+  end
+
+
+private
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
