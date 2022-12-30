503c62d77e38654b0725ef404d5f9f5ceb3f2e37
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-12-30 17:20:20 -0500

    Create login form and path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
   
+  def new
+  end
+
   def create
-    
+    ##login 
   end
   
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'users#show'
 
   get '/logout', to: 'sessions#destroy' #can't get this button to route correctly with bootstrap/delete verb...will come back to it.
-  get '/login', to: 'sessions#create'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
