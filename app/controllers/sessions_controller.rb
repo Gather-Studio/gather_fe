13bf65c9a814289023368960e73032709b872458
Jennifer Halloran <jenniferlhalloran@gmail.com>
2023-01-04 14:19:23 -0500

    Complete user session/login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    ##login 
+    user = SessionFacade.create_session(login_params)
+    if user.is_a?(Error)
+      flash[:alert] = user.message
+      redirect_to '/login'
+    elsif user.is_a?(User)
+      session[:id] = user.id
+      redirect_to '/dashboard'
+    end
   end
   
   def destroy
     session.destroy
     redirect_to '/'
+  end
+
+
+private
+  def login_params
+    params.permit(:email, :password)
   end
 end
