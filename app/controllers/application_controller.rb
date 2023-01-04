47f23bfad561370b225af785ecf66da5fcb2f0cd
Jennifer Halloran <jenniferlhalloran@gmail.com>
2023-01-04 15:41:19 -0500

    Create view for user dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user 
-    UserFacade.get_user(session[:id]) if session[:user_id]
+    UserFacade.get_user(session[:id]) if session[:id]
   end
 
     def authorize_user
