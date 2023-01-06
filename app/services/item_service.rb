e1b1c03ad2920474ac44a790138f2315b3744f9d
Jennifer Halloran <jenniferlhalloran@gmail.com>
2023-01-06 17:17:48 -0500

    Create base service

--- app/services/item_service.rb
+++ app/services/item_service.rb
@@ -1,21 +1,13 @@
 class ItemService
-  def self.connection 
-    # Faraday.new(url: 'https://gatherbe.fly.dev')
-    Faraday.new(url: 'http://localhost:3001')
-  end
-
-  def self.get_json(response)
-    JSON.parse(response.body, symbolize_names: true)
-  end
 
   def self.create_item(data, user_id)
     formatted_data = {"item"=> data}
-    response = connection.post("/api/v1/users/#{user_id}/items", formatted_data.to_json, {'Content-Type'=>'application/json'})
-    get_json(response)
+    response = BaseService.connection.post("/api/v1/users/#{user_id}/items", formatted_data.to_json, {'Content-Type'=>'application/json'})
+    BaseService.get_json(response)
   end
 
   def self.get_user_items(id)
-    response = connection.get("api/v1/users/#{id}/items")
-    get_json(response)
+    response = BaseService.connection.get("api/v1/users/#{id}/items")
+    BaseService.get_json(response)
   end
 end
--- app/services/session_service.rb
+++ app/services/session_service.rb
@@ -1,15 +1,7 @@
 class SessionService 
-    def self.connection 
-    # Faraday.new(url: 'https://gatherbe.fly.dev')
-    Faraday.new(url: 'http://localhost:3001')
-  end
-
-  def self.get_json(response)
-    JSON.parse(response.body, symbolize_names: true)
-  end
 
   def self.create_session(data)
-    response = connection.post("/api/v1/sessions", data.to_json, {'Content-Type'=>'application/json'})
-    get_json(response)
+    response = BaseService.connection.post("/api/v1/sessions", data.to_json, {'Content-Type'=>'application/json'})
+    BaseService.get_json(response)
   end
 end
